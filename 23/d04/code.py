fe642127d3dc80da1a63ea6ffc7546057eb3be08
noone <polferdman.l@gmail.com>
2023-12-09 21:48:43 +0100

    d04 fix problem with data

--- 23/d04/code.py
+++ 23/d04/code.py
@@ -5,23 +5,20 @@ for l in f:
     numbers = l.split(':')[1].split('|')
 
     wining_numbers = numbers[0].split(' ')
-    del wining_numbers[0]
-    del wining_numbers[-1]
+    wining_numbers = [x for x in wining_numbers if x != '']
+
     card_numbers = numbers[1].split(' ')
-    del card_numbers[0]
-    del card_numbers[-1]
+    card_numbers = [x for x in card_numbers if x != '']
+    card_numbers[-1] = card_numbers[-1].replace('\n','')
     
 
     count = 0
     for wining_num in wining_numbers:
         if wining_num in card_numbers:
             count = count + 1
-    if count > 2:
-        res = 1
-        #count = 2 ** (count-1)
-        print(count)
-        for x in range(count):
-            res = res * 2
-    sum = sum + count
+    res = count
+    if res != 0:
+        res = 2 ** (res-1)
+    sum = sum + res
         
 print(sum) 
